f45c77278f2434f5def9eff9efc8eff035d32383
Wesley Ordoñez <wesordonez1@gmail.com>
2024-09-12 21:09:35 -0500

    added header dissapear functionality, needs fade transition

--- dunosis_site/landing/static/landing/js/script.js
+++ dunosis_site/landing/static/landing/js/script.js
@@ -10,3 +10,19 @@ document.addEventListener('DOMContentLoaded', function() {
         nextSection.scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+// Hide navbar, show on scroll down
+
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header');
+    const nextSection = document.querySelector('#services-section');
+
+    window.addEventListener('scroll', function() {
+        const sectionTop = nextSection.getBoundingClientRect().top;
+        if (sectionTop <= 0) {
+            header.style.display = 'flex';
+        } else {
+            header.style.display = 'none';
+        }
+    });
+});
