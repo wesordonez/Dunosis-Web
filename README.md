eea3f2326edf1e944a4abaace73da96c4d5e0d3f
Wesley Ordoñez <wesordonez1@gmail.com>
2024-09-13 20:52:50 -0500

    improved readme, added .env changed settings.py

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# Dunosis-Web
-This repo contains the main website for Dunosis LLC
+# Dunosis-Website
+Main website for Dunosis LLC. 
+
+## Description
+Dunosis is the official website for a dynamic web development agency specializing in creating tailored digital solutions for businesses of all sizes. Built using Django, the site showcases our expertise in developing high-performance web applications, delivering responsive designs, and ensuring smooth deployment processes. Our agency is committed to helping clients bring their vision to life with clean, maintainable code and cutting-edge technologies, including cloud deployment on Digital Ocean and robust database management with Postgres. Explore our services, portfolio, and innovative approach through the Dunosis website.
+
+## Installation
+To install the project, clone the repository and run the following command:
+
+```bash
+pip install requirements.txt
+```
+
+## Usage
+To start the application:
+
+1. Navigate to the root django project directory
+
+```zsh
+cd dunosis_site
+```
+
+2. Next, run the runserver command
+
+```zsh
+python manage.py runserver
+```
+
+## Roadmap
+
+This is the rough timeline and roadmap for the website:
+
+1. Backend work, database stuff...
+2. Contact Page with contact submission form
+3. Richer feature descriptions and images
+4. Pricing page and structure
+5. Calendar and scheduling features
+6. AI Chatbot for questions
+7. More to come... 
+
+## Contributing
+1. Fork the repository.
+2. Create a new branch (`git checkout -b feature/YourFeature`).
+3. Make your changes.
+4. Commit your changes (`git commit -m 'Add some feature'`).
+5. Push to the branch (`git push origin feature/YourFeature`).
+6. Open a pull request.
+
--- dunosis_site/dunosis_site/settings.py
+++ dunosis_site/dunosis_site/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
+import os
+from dotenv import load_dotenv
+# dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g*(a8#4#st00wekq^%wmc(21&-jlks@*rm@^nii8@la&f5c1a!'
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
+ALLOWED_HOSTS = [host.strip() for host in ALLOWED_HOSTS]
 
 
 # Application definition
@@ -74,12 +81,28 @@ WSGI_APPLICATION = 'dunosis_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('DJANGO_ENV') == 'production':
+    DATABASES = {
+        'default': {
+            'ENGINE': os.environ.get('PRD_SQL_ENGINE'),
+            'NAME': os.environ.get('PRD_SQL_NAME'),
+            'USER': os.environ.get('PRD_SQL_USER'),
+            'PASSWORD': os.environ.get('PRD_SQL_PASSWORD'),
+            'HOST': os.environ.get('PRD_SQL_HOST'),
+            'PORT': os.environ.get('PRD_SQL_PORT')
+        }
     }
-}
+else:
+    DATABASES = {
+            'default': {
+                'ENGINE': os.environ.get('SQL_ENGINE'),
+                'NAME': os.environ.get('SQL_NAME'),
+                'USER': os.environ.get('SQL_USER'),
+                'PASSWORD': os.environ.get('SQL_PASSWORD'),
+                'HOST': os.environ.get('SQL_HOST'),
+                'PORT': os.environ.get('SQL_PORT')
+            }
+        }
 
 
 # Password validation
@@ -117,6 +140,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "landing/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
