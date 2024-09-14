669a8e164290d6f6e1971741b367aa603ce7e075
Wesley Ordoñez <wesordonez1@gmail.com>
2024-09-14 12:01:32 -0500

    adjusted wsgi path

--- dunosis_site/dunosis_site/wsgi.py
+++ dunosis_site/dunosis_site/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dunosis_site.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dunosis_site.dunosis_site.settings')
 
 application = get_wsgi_application()
