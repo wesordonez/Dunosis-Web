aa51308c3cdeb671491c9f90da85dca9b1496891
Wesley Ordoñez <wesordonez1@gmail.com>
2024-09-16 22:16:15 -0500

    added / to static_root

--- dunosis_site/dunosis_site/settings.py
+++ dunosis_site/dunosis_site/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "landing/static",
 ]
